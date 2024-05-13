ef816f48fb016ae180dab150e7e279dee6aea20e
Sean-Vu <seanvu123@gmail.com>
2024-05-13 16:43:58 -0700

    Rename savantApp folder into savantAPIs and add rest framework package

--- savant/savant/settings.py
+++ savant/savant/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "savantApp"
+    "savantAPIs"
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
